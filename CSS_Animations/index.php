4cca4dd4d2c868690a17df9ee258f9d1dfbcbb34
Fernando Mendoza <fernandoza@gmail.com>
2016-08-05 15:06:05 -0500

    CSS Animations code

---
+++ CSS_Animations/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Loose Arts | Web Development</title>
+	<meta charset="utf-8">
+	<meta name="description" content="Web development business in Mexico, USA and KSA, we operate worldwide.">
+	<link rel="stylesheet" type="text/css" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/index.css">
+</head>
+<body>
+	<div class="container">
+		<div id="index-div">
+			<img id="frame" src="img/frame.png">
+			<img id="logo" src="img/logo.png">
+			<img id="big-circle" src="img/circle.png">
+			<div id="menu">
+				<ol>
+					<li>
+						<a href="portfolio.html">Portfolio</a>
+						<span class="menu-bg"></span>
+					</li>
+					<li>
+						<a href="about_us.html">About Us</a>
+						<span class="menu-bg"></span>
+					</li>
+					<li>
+						<a href="contact.html">Contact</a>
+						<span class="menu-bg"></span>
+					</li>
+				</ol>
+			</div>
+			<img class="circles" id="circle1" src="img/circle.png">
+			<img class="circles" id="circle2" src="img/circle.png">
+			<img class="circles" id="circle3" src="img/circle.png">
+			<img class="circles" id="circle4" src="img/circle.png">
+			<img class="circles" id="circle5" src="img/circle.png">
+			<footer>
+				<span>LOOSEARTS.COM</span>
+				<br>
+				<span>2008 - <?= date('Y')?> (C)</span>
+			</footer>
+		</div>
+	</div>
+</body>
+</html>
