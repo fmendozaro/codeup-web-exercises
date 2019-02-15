cf4800158637a499c0903d9aec5d67450382324a
fer <fernandoza@gmail.com>
2019-02-15 10:28:57 -0600

    feat: add options to center a map

--- bing/map.js
+++ bing/map.js
@@ -1,5 +1,10 @@
 var map;
 
 function loadMapScenario() {
-    map = new Microsoft.Maps.Map(document.getElementById('myMap'), {});
+    map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
+        /* No need to set credentials if already passed in URL */
+        center: new Microsoft.Maps.Location(51.50632, -0.12714),
+        mapTypeId: Microsoft.Maps.MapTypeId.aerial,
+        zoom: 10
+    });
 }
